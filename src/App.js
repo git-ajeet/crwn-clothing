2a3530cf1502574e197cabbd765ae4344a995591
Ajeet Chauhan <ajeet.chauhan0994@gmail.com>
2021-06-28 18:47:10 +0530

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
+
 
 import HomePage from './pages/homepage/Homepage.component';
 
+function Hats() {
+  return (
+    <div>
+      <h1>Hats</h1>
+    </div>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Route exact path='/' component={HomePage}></Route>
+      <Route path='/shop/hats' component={Hats}></Route>
     </div>
   );
 }
